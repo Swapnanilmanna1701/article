c03c97db7c711d85777ede7d0910d1b46d2e72b6
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2025-02-07 19:39:17 +0530

    fix: centered layout and hero gradient changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Hero from "@/components/Hero";
 import Demo from "@/components/Demo";
+import Hero from "@/components/Hero";
 
 import "./globals.css";
 
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,3 @@
-import React from "react";
-
-//import { logo } from "../assets";
-
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
